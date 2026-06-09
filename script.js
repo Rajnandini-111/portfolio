c118a089efab963630448a1fc159916baae2aff1
Rajnandini-111 <95406610+Rajnandini-111@users.noreply.github.com>
2026-06-09 23:22:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Portfolio Loaded Successfully");
+
+new Typed("#typing",{
+strings:[
+"Senior Web Developer",
+"WordPress Developer",
+"Shopify Developer",
+"Framer Developer"
+],
+typeSpeed:70,
+backSpeed:50,
+loop:true
+});
